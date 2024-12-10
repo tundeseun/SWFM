c6d433fcd08db8f3d9ee3d561ba95b6c7eab697c
Habeeb O. Olaitan <olaitan6201@gmail.com>
2024-12-10 08:34:55 +0100

    modify `Shelf` model and migration

--- app/Models/Shelf.php
+++ app/Models/Shelf.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Shelf extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
