c9023868eca4fa5d768fe251ce1f50b88db83f22
Tunde Abiona <tunde.abiona@gmail.com>
2024-12-17 18:05:01 +0100

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Vue App</title>
+    <title>SWFM ERP Solution - SouthWest Farmers Market</title>
     @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- Include compiled CSS and JS -->
 </head>
 <body>
